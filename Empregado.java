47bdd6a74236f3fc8a10d00d163e739a4288fe7f
Fernando Fusada <fernandofusada.8604@aluno.saojudas.br>
2022-09-15 21:18:42 -0300

    Cálculo de salário efetuado de acordo com o tipo.

---
+++ Empregado.java
@@ -0,0 +1,37 @@
+public class Empregado {
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    private String nome;
+    private int idade;
+
+    public double calculaSalario() {
+        if(tipo == 1)
+            return salario;
+        else if(tipo == 2)
+            return salario + salario * comissao;
+        else if(tipo == 3)
+            return salario + bonus;
+        else
+            return 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+
+}
